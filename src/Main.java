be51fef100232a78305992859b531a4dcf3c60ce
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2024-05-31 17:35:28 +0900

    Add some test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import math.shape.Bar;
+import math.shape.Cube;
+import math.shape.Cylinder;
+import math.utils.VolumeCalculator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Expects "Cube Volume: 27"
+        Cube cube = new Cube(3);
+        System.out.println("Cube Volume: " + VolumeCalculator.calculateVolume(cube));
+
+        // Expects "Bar Volume: 60"
+        Bar bar = new Bar(3, 4, 5);
+        System.out.println("Bar Volume: " + VolumeCalculator.calculateVolume(bar));
+
+        // Expects "Cylinder Volume: 3140"
+        Cylinder cylinder = new Cylinder(10, 10);
+        System.out.println("Cylinder Volume: " + VolumeCalculator.calculateVolume(cylinder));
     }
 }
